8b7cf0017352c2e6493d3e1e81d05dc7cae0d4e1
nan <msg@nancode.cn>
2021-03-17 12:22:28 +0800

    update title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -4,7 +4,7 @@ const now = dayjs().format("YYYY-MM-DD HH:mm:ss");
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
   title: "Dora 哆啦",
-  tagline: "简单、易部署、可扩展的前端监控平台",
+  tagline: "轻量、易部署、可扩展的前端监控平台",
   url: "https://nancode.cn",
   baseUrl: "/",
   onBrokenLinks: "throw",
