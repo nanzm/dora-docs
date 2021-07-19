5fa8becfe27dc808deca92762b60b37e633de19c
nan <msg@nancode.cn>
2021-07-19 11:56:05 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -92,12 +92,12 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: "https://github.com/dora-projects/dora-docs/edit/master/",
+          editUrl: "https://github.com/dora-projects/docs/edit/master/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
-          editUrl: "https://github.com/dora-projects/dora-docs/edit/master/",
+          editUrl: "https://github.com/dora-projects/docs/edit/master/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
